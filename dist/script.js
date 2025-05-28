24bb68d2e594937d36cdb7adfea1863a3664a179
metax0116 <2410017@i-seifu.jp>
2025-05-28 15:34:03 +0900

    変更

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,8 @@
 // Supabase初期化
 const SUPABASE_URL = 'https://vguesgqyjpohphmeyiaf.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZndWVzZ3F5anBvaHBobWV5aWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzOTc2ODAsImV4cCI6MjA2Mzk3MzY4MH0.JZes7O8Q3naGO7RAHzCpIJ4NMRvHkmgw1fCfGLN4MUM';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const APP_PREFIX = 'coupleApp_';
 
